0a3218cc81563040713872b6298c88b67b535697
WhyBeU <yoann.buratti@gmail.com>
2022-09-06 10:46:12 +1000

    Initial commit

---
+++ code/statistics.py
@@ -0,0 +1,33 @@
+# %%--  Import
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import datetime
+
+from matplotlibstyle import *
+# %%-
+
+# %%--  Settings
+DATADIR = "data/"
+FIGDIR = "figures/"
+SAVE = True
+# %%-
+
+# %%--  Data loading
+filenames = [
+    "ACCIDENT_CHAINAGE",
+    "ACCIDENT_EVENT",
+    "ACCIDENT_LOCATION",
+    "ACCIDENT",
+    "ATMOSPHERIC_COND",
+    "NODE_ID_COMPLEX_INT_ID",
+    "NODE",
+    "PERSON",
+    "SUBDCA",
+    "VEHICLE"
+]
+dfs_dic = {}
+for filename in filenames:
+    dfs_dic[filename] = pd.read_csv(DATADIR+filename+".csv")
+# %%-
