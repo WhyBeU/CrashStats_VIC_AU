c1eadf3ee71991a252e53d2c5940f337c6fdd65a
WhyBeU <yoann.buratti@gmail.com>
2022-09-07 14:44:56 +1000

    Added descending order for categorical injury statistics

--- code/statistics.py
+++ code/statistics.py
@@ -86,43 +86,43 @@ A2_df = dfs_dic["ACCIDENT"].loc[dfs_dic["ACCIDENT"]["SEVERITY"]<3].copy(deep=Tru
 
 #   Merge to atmospheric condition dataset keep only first sequend of events to asses initial impact data
 A2_df_atms = dfs_dic['ATMOSPHERIC_COND'].loc[dfs_dic['ATMOSPHERIC_COND']['ATMOSPH_COND_SEQ'] == 1].copy(deep=True)
-A2_df = pd.merge(A2_df_fatal, A2_df_atms,how="left", on="ACCIDENT_NO")
+A2_df = pd.merge(A2_df, A2_df_atms,how="left", on="ACCIDENT_NO")
 
 #   Convert to injury rate percentage
-A2_df['NO_PERSONS_KILLED']=A2_df['NO_PERSONS_KILLED']/A2_df['NO_PERSONS_KILLED'].sum()*100
-A2_df['NO_PERSONS_INJ_2']=A2_df['NO_PERSONS_INJ_2']/A2_df['NO_PERSONS_INJ_2'].sum()*100
+A2_df['NO_PERSONS_KILLED'] = A2_df['NO_PERSONS_KILLED']/A2_df['NO_PERSONS_KILLED'].sum()*100
+A2_df['NO_PERSONS_INJ_2'] = A2_df['NO_PERSONS_INJ_2']/A2_df['NO_PERSONS_INJ_2'].sum()*100
 
 #   Reshape data for plot
-A2_df_speed=A2_df[['NO_PERSONS_KILLED','NO_PERSONS_INJ_2','SPEED_ZONE']].groupby("SPEED_ZONE").sum()
+A2_df_speed = A2_df[['NO_PERSONS_KILLED','NO_PERSONS_INJ_2','SPEED_ZONE']].groupby("SPEED_ZONE").sum()
 A2_df_speed.reset_index(inplace=True)
 A2_df_speed = A2_df_speed.melt(id_vars="SPEED_ZONE",var_name='Injury level',value_name='Number of persons [%]')
 A2_df_speed.replace({'Injury level':{'NO_PERSONS_KILLED':'Killed','NO_PERSONS_INJ_2':'Serious injury'}}, inplace=True)
 
-A2_df_day=A2_df[['NO_PERSONS_KILLED','NO_PERSONS_INJ_2','Day Week Description']].groupby("Day Week Description").sum()
+A2_df_day = A2_df[['NO_PERSONS_KILLED','NO_PERSONS_INJ_2','Day Week Description']].groupby("Day Week Description").sum()
 A2_df_day.reset_index(inplace=True)
 A2_df_day = A2_df_day.melt(id_vars="Day Week Description",var_name='Injury level',value_name='Number of persons [%]')
 A2_df_day.replace({'Injury level':{'NO_PERSONS_KILLED':'Killed','NO_PERSONS_INJ_2':'Serious injury'}}, inplace=True)
 
-A2_df_type=A2_df[['NO_PERSONS_KILLED','NO_PERSONS_INJ_2','Accident Type Desc']].groupby("Accident Type Desc").sum()
+A2_df_type = A2_df[['NO_PERSONS_KILLED','NO_PERSONS_INJ_2','Accident Type Desc']].groupby("Accident Type Desc").sum()
 A2_df_type.reset_index(inplace=True)
 A2_df_type = A2_df_type.melt(id_vars="Accident Type Desc",var_name='Injury level',value_name='Number of persons [%]')
 A2_df_type.replace({'Injury level':{'NO_PERSONS_KILLED':'Killed','NO_PERSONS_INJ_2':'Serious injury'}}, inplace=True)
 
-A2_df_light=A2_df[['NO_PERSONS_KILLED','NO_PERSONS_INJ_2','Light Condition Desc']].groupby("Light Condition Desc").sum()
+A2_df_light = A2_df[['NO_PERSONS_KILLED','NO_PERSONS_INJ_2','Light Condition Desc']].groupby("Light Condition Desc").sum()
 A2_df_light.reset_index(inplace=True)
 A2_df_light = A2_df_light.melt(id_vars="Light Condition Desc",var_name='Injury level',value_name='Number of persons [%]')
 A2_df_light.replace({'Injury level':{'NO_PERSONS_KILLED':'Killed','NO_PERSONS_INJ_2':'Serious injury'}}, inplace=True)
 
-A2_df_road=A2_df[['NO_PERSONS_KILLED','NO_PERSONS_INJ_2','Road Geometry Desc']].groupby("Road Geometry Desc").sum()
+A2_df_road = A2_df[['NO_PERSONS_KILLED','NO_PERSONS_INJ_2','Road Geometry Desc']].groupby("Road Geometry Desc").sum()
 A2_df_road.reset_index(inplace=True)
 A2_df_road = A2_df_road.melt(id_vars="Road Geometry Desc",var_name='Injury level',value_name='Number of persons [%]')
 A2_df_road.replace({'Injury level':{'NO_PERSONS_KILLED':'Killed','NO_PERSONS_INJ_2':'Serious injury'}}, inplace=True)
 
-A2_df_atm=A2_df[['NO_PERSONS_KILLED','NO_PERSONS_INJ_2','Atmosph Cond Desc']].groupby("Atmosph Cond Desc").sum()
+A2_df_atm = A2_df[['NO_PERSONS_KILLED','NO_PERSONS_INJ_2','Atmosph Cond Desc']].groupby("Atmosph Cond Desc").sum()
 A2_df_atm.reset_index(inplace=True)
 A2_df_atm = A2_df_atm.melt(id_vars="Atmosph Cond Desc",var_name='Injury level',value_name='Number of persons [%]')
 A2_df_atm.replace({'Injury level':{'NO_PERSONS_KILLED':'Killed','NO_PERSONS_INJ_2':'Serious injury'}}, inplace=True)
-A2_df_atm=A2_df_atm.loc[A2_df_atm["Atmosph Cond Desc"]!="Not Applicable"]
+A2_df_atm = A2_df_atm.loc[A2_df_atm["Atmosph Cond Desc"]!="Not Applicable"]
 
 #   Plot
 figname = "Injury rate by category"
@@ -131,7 +131,9 @@ x_tab = ['SPEED_ZONE', 'Day Week Description', 'Accident Type Desc', 'Light Cond
 df_tab = [A2_df_speed, A2_df_day, A2_df_type, A2_df_light, A2_df_road, A2_df_atm]
 xlabel_tab = ['Speed zone', 'Day of the week', 'Accident type', 'Light condition', 'Road type', 'Atmospheric condition']
 rot_tab = [0, 0, 90, 90, 90, 90]
-for x, df, xlabel, rot, ax in zip(x_tab, df_tab, xlabel_tab, rot_tab, axes.flatten()):
+asc_tab = [False, False, True, True, True, True]
+for x, df, xlabel, rot, ax, asc in zip(x_tab, df_tab, xlabel_tab, rot_tab, axes.flatten(),asc_tab):
+    if asc: df.sort_values('Number of persons [%]', inplace=True, ascending=False)
     sns.barplot(
         x=x,
         y='Number of persons [%]',
