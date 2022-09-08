f3eb40049a62b7be9592b638acd729931c5aeccb
WhyBeU <yoann.buratti@gmail.com>
2022-09-08 13:52:12 +1000

    Bendigo serious accident map

--- code/location_statistics.py
+++ code/location_statistics.py
@@ -76,3 +76,36 @@ if SAVE: plt.savefig(FIGDIR+datetime.datetime.now().strftime("%Y-%m-%d-%H-%M")+"
 plt.tight_layout()
 plt.show()
 # %%-
+# %%--  1-Map of Bendigo
+#   Fatal and serious injury datasets [Severity 1 or 2]
+B1_df = dfs_dic["ACCIDENT"].loc[dfs_dic["ACCIDENT"]["SEVERITY"]<3].copy(deep=True)
+
+#   Merge to NODE dataset to access location. Remove ACCIDENT_NO duplicates
+B1_df_node = dfs_dic['NODE'].copy(deep=True)
+B1_df_node.drop_duplicates(subset=['ACCIDENT_NO'], inplace=True)
+B1_df = pd.merge(B1_df, B1_df_node,how="left", on="ACCIDENT_NO")
+B1_df['TOTAL_INJURY'] = B1_df['NO_PERSONS_KILLED'] + B1_df['NO_PERSONS_INJ_2']
+
+#   Convert to injury rate percentage
+B1_df['TOTAL_INJURY'] = B1_df['TOTAL_INJURY']/B1_df['TOTAL_INJURY'].sum()*100
+
+#   Plot map of all serious accidents
+figname = "Bendigo injury and fatality map"
+alpha = 0.5
+size = 5
+xmin = 2.41e6
+ymin = 2.505e6
+window = 0.045e6
+
+fig, (ax1) = plt.subplots(nrows=1, ncols=1, figsize=(5,5))
+ax1.scatter(B1_df["VICGRID94_X"],B1_df["VICGRID94_Y"], marker=".", s=size, alpha=alpha, c="orchid")
+
+ax1.set_xlim(xmin, xmin+window)
+ax1.set_ylim(ymin, ymin + window)
+ax1.set_aspect("equal")
+ax1.axis("off")
+
+if SAVE: plt.savefig(FIGDIR+datetime.datetime.now().strftime("%Y-%m-%d-%H-%M")+"_"+figname+".png",transparent=True,bbox_inches='tight')
+plt.tight_layout()
+plt.show()
+# %%-
